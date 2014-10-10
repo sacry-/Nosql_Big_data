fbd508652233ebb0df20f6d0d7bf81828ddacd14
sacry- <nitsche25@gmail.com>
2014-10-10 15:33:10 +0200

    afugabe4 finished redis

--- a2/a4_redis_key_value.py
+++ a2/a4_redis_key_value.py
@@ -1,43 +1,50 @@
 import redis
+import ast
 
+def string_to_hash(s):
+  return ast.literal_eval(s)
 
-def create_redis(host="localhost", kvs=("1337", 1337)):
+# aufgabe 4a
+def load_file(apath):
+  f = open(apath, 'r')
+  result = []
+  for line in f:
+    result.append(string_to_hash(line))
+  f.close()
+  return result
+
+def populate_redis_with_hash(r_server, h):
+  result = []
+  for elem in h:
+    r_server.set(elem["_id"], elem)
+    r_server.rpush(elem["city"], elem["_id"])
+  print "Populated!"
+
+def create_redis():
   r_server = redis.Redis('localhost')
-  r_server.set(kvs[0], kvs[1])                                  
+  if not r_server.exists("13063"):
+    h = load_file("/Users/sacry/dev/uni/s5/nosql/Nosql_Big_data/a2/plz.data")
+    populate_redis_with_hash(r_server, h)
   return r_server
 
-def load_file():
+
+# aufgabe 4b
+def get_city_and_state(r_server, plz):
+  res = string_to_hash(r_server.get(plz))
+  return (res["city"], res["state"])
+
+# aufgabe 4c
+def plz_for_town(r_server, town):
+  return r_server.lrange(town, 0, r_server.llen(town))
+
+r_server = create_redis()
+print get_city_and_state(r_server, "13063")
+
+print plz_for_town(r_server, "HAMBURG")
+print plz_for_town(r_server, "TUMTUM")
 
 
-def fill_redis_with_file_content():
 
 
-r_server.set('counter', 1)
-r_server.incr('counter')
 
-r_server.decr('counter') #we decrease the key value by 1, has to be int
-print 'the counter was decreased! '+ r_server.get('counter') #the key is back to normal
-
-
-'''Now we are ready to jump into another redis data type, the list, notice 
-that they are exactly mapped to python lists once you get them'''
-
-r_server.rpush('list1', 'element1') #we use list1 as a list and push element1 as its element
-
-r_server.rpush('list1', 'element2') #assign another element to our list
-r_server.rpush('list2', 'element3') #the same
-
-print 'our redis list len is: %s'% r_server.llen('list1') #with llen we get our redis list size right from redis
-
-print 'at pos 1 of our list is: %s'% r_server.lindex('list1', 1) #with lindex we query redis to tell us which element is at pos 1 of our list
-
-'''sets perform identically to the built in Python set type. Simply, sets are lists but, can only have unique values.'''
-
-r_server.sadd("set1", "el1")
-r_server.sadd("set1", "el2")
-r_server.sadd("set1", "el2")
-
-print 'the member of our set are: %s'% r_server.smembers("set1")
-
-'''basically our redis client can do any command supported by redis, check out redis documentation for available commands for your server'''
 
