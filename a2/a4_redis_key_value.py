091291e126a3a8da2d76506d7a66579fe413c616
wiltherr <wittler.tim@gmail.com>
2014-10-20 14:15:15 +0200

    Text GUI added to a4 redis

--- a2/a4_redis_key_value.py
+++ a2/a4_redis_key_value.py
@@ -1,50 +1,71 @@
 import redis
 import ast
 
-def string_to_hash(s):
-  return ast.literal_eval(s)
+#decode JSON
+def string_to_hash(json_string):
+	return ast.literal_eval(json_string)
 
 # aufgabe 4a
+#open file and decode json lines
 def load_file(apath):
-  f = open(apath, 'r')
-  result = []
-  for line in f:
-    result.append(string_to_hash(line))
-  f.close()
-  return result
+	f = open(apath, 'r')
+  	result = []
+  	for line in f:
+		result.append(string_to_hash(line))
+  	f.close()
+  	return result
 
+#add Hash h to Redis
 def populate_redis_with_hash(r_server, h):
-  result = []
-  for elem in h:
-    r_server.set(elem["_id"], elem)
-    r_server.rpush(elem["city"], elem["_id"])
-  print "Populated!"
+	result = []
+	for elem in h:
+		r_server.set(elem["_id"], elem)
+		r_server.rpush(elem["city"], elem["_id"])
+	print "Populated!"
 
+#connect to redis and load data if not in db
 def create_redis():
-  r_server = redis.Redis('localhost')
-  if not r_server.exists("13063"):
-    h = load_file("/Users/sacry/dev/uni/s5/nosql/Nosql_Big_data/a2/plz.data")
-    populate_redis_with_hash(r_server, h)
-  return r_server
+	r_server = redis.Redis('localhost')
+	if not r_server.exists("13063"):
+		print "No Data exists, importing plz.data..."
+		h = load_file("./plz.data")
+		populate_redis_with_hash(r_server, h)
+	return r_server
 
 
 # aufgabe 4b
-def get_city_and_state(r_server, plz):
-  res = string_to_hash(r_server.get(plz))
-  return (res["city"], res["state"])
+def get_city_and_state(r_server, plz=""):
+	if(plz == ""):
+		plz = raw_input("Bitte eine PLZ angeben: ")
+
+	res = []
+	res = string_to_hash(r_server.get(plz))
+  	return (res["city"], res["state"])
 
 # aufgabe 4c
-def plz_for_town(r_server, town):
-  return r_server.lrange(town, 0, r_server.llen(town))
-
-r_server = create_redis()
-print get_city_and_state(r_server, "13063")
-
-print plz_for_town(r_server, "HAMBURG")
-print plz_for_town(r_server, "TUMTUM")
+def plz_for_town(r_server, town=""):
+	if(town==""):
+		town = raw_input("Bitte eine Stadt angeben: ")
+	return r_server.lrange(town, 0, r_server.llen(town))
 
 
+#gui
+def gui_start():
+	r_server = create_redis()
+	command = ""
+	while command != "exit":
+		if command == "GETPLZ":
+			print "Postleitzahl: %s" %plz_for_town(r_server)
+		elif command == "GETTOWN":
+			res = get_city_and_state(r_server)
+			print "Stadt: {0} | Staat: {1}".format(res[0],res[1])
+		else:
+			print "Kommandos: GETPLZ, GETTOWN, exit"
+		command = raw_input()
+	print "Beendet."
 
-
-
-
+gui_start() 
+	
+#print get_city_and_state(r_server, "13063")
+#print plz_for_town(r_server, "HAMBURG")
+#print plz_for_town(r_server, "TUMTUM")
