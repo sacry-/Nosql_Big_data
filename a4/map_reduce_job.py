b2ae02d012be7ba1ac8f41a9e68aa2809bdcdd4e
sacry- <nitsche25@gmail.com>
2014-12-04 12:56:53 +0100

    fixed path bug

--- a4/map_reduce_job.py
+++ a4/map_reduce_job.py
@@ -31,10 +31,10 @@ def execute(copy_from_local, map_reduce, copy_to_local):
 
 
 nosql_dir = os.path.dirname(os.path.realpath(__file__))
-nosql_files = "%s/files/" % nosql_dir
+nosql_files = "%s/files" % nosql_dir
 config = {
-  "input_path" : "%sinput.txt" % nosql_files,
-  "output_path" : "%soutput.txt" % nosql_files,
+  "input_path" : "%s/input.txt" % nosql_files,
+  "output_path" : "%s/output.txt" % nosql_files,
   "mapper" : "%s/mapper.py" % nosql_files,
   "reducer" : "%s/reducer.py" % nosql_files,
   "hadoop_dir" : "/usr/local/hadoop/bin/hadoop",
