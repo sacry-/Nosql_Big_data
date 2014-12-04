2bb8d5993c0075751ebd61985189e02ba9093aeb
sacry- <nitsche25@gmail.com>
2014-12-04 13:36:09 +0100

    abstracted map reduce config

--- a4/map_reduce_job.py
+++ a4/map_reduce_job.py
@@ -1,5 +1,49 @@
 
 
+def parse_args():
+  from argparse import ArgumentParser
+  parser = ArgumentParser(description="start map reduce job")
+  parser.add_argument('-nosql_dir', action='store', metavar="NOSQL_PROJECT", help="nosql project dir")
+  parser.add_argument('-hadoop_home', action='store', metavar="HADOOP_HOME", help="hadoop excute dir")
+  parser.add_argument('-stream_jar', action='store', metavar="HADOOP_STREAM", help="hadoop streaming jar")
+  args = parser.parse_args()
+  return (args.nosql_dir, args.hadoop_home, args.stream_jar)
+
+
+class Config():
+
+  def __init__(self, nosql_dir=None, hadoop_home=None, stream_jar=None):
+    if not nosql_dir: 
+      from os import path
+      nosql_dir = path.dirname(path.realpath(__file__))
+    self.nosql_dir = nosql_dir
+    if not hadoop_home: 
+      hadoop_home = "/usr/local/hadoop/bin/hadoop"
+    self.hadoop_home = hadoop_home
+    if not stream_jar: 
+      stream_jar = "/usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.4.0.jar"
+    self.stream_jar = stream_jar
+
+  def __repr__(self):
+    result = []
+    for k, v in self.to_hash().iteritems():
+      tmp = "%s: %s" % (k, v)
+      result.append(tmp)
+    return "\n".join(result)
+
+  def to_hash(self):
+    nosql_files = "%s/files" % self.nosql_dir
+    config = {
+      "input_path" : "%s/input.txt" % nosql_files,
+      "output_path" : "%s/output.txt" % nosql_files,
+      "mapper" : "%s/mapper.py" % nosql_files,
+      "reducer" : "%s/reducer.py" % nosql_files,
+      "hadoop_dir" : self.hadoop_home,
+      "path_to_streaming_jar" : self.stream_jar
+    }
+    return config
+
+
 def create_job(config):
   hadoop_dir = config["hadoop_dir"]
   path_to_streaming_jar = config["path_to_streaming_jar"]
@@ -29,47 +73,16 @@ def execute(copy_from_local, map_reduce, copy_to_local):
   call(copy_to_local)
 
 
-def create_config(nosql_dir=None, hadoop_home=None, stream_jar=None):
-  if not nosql_dir: 
-    from os import path
-    nosql_dir = path.dirname(path.realpath(__file__))
-  if not hadoop_home: 
-    hadoop_home = "/usr/local/hadoop/bin/hadoop"
-  if not stream_jar: 
-    stream_jar = "/usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.4.0.jar"
-
-  print "nosql dir: %s" % nosql_dir
-  print "hadoop home: %s" % hadoop_home
-  print "streaming jar: %s" % stream_jar
-
-  nosql_files = "%s/files" % nosql_dir
-  config = {
-    "input_path" : "%s/input.txt" % nosql_files,
-    "output_path" : "%s/output.txt" % nosql_files,
-    "mapper" : "%s/mapper.py" % nosql_files,
-    "reducer" : "%s/reducer.py" % nosql_files,
-    "hadoop_dir" : hadoop_home,
-    "path_to_streaming_jar" : stream_jar
-  }
-  return config
-
-
-def parse_args():
-  from argparse import ArgumentParser
-  parser = ArgumentParser(description="start map reduce job")
-  parser.add_argument('-nosql_dir', action='store', metavar="NOSQL_PROJECT", help="nosql project dir")
-  parser.add_argument('-hadoop_home', action='store', metavar="HADOOP_HOME", help="hadoop excute dir")
-  parser.add_argument('-stream_jar', action='store', metavar="HADOOP_STREAM", help="hadoop streaming jar")
-  args = parser.parse_args()
-  return (args.nosql_dir, args.hadoop_home, args.stream_jar)
-
-
 # uses defaults: python map_reduce_job.py
 # uses provided paths: python map_reduce_job.py -hadoop_home=abc/de -stream_jar=abc/de
 def run_job():
+  print "configuring..."
   nosql_dir, hadoop_home, stream_jar = parse_args()
-  config = create_config(nosql_dir, hadoop_home, stream_jar)
-  copy_from_local, map_reduce, copy_to_local = create_job(config)
+  config = Config(nosql_dir, hadoop_home, stream_jar)
+  print config
+
+  print "executing map/reduce job.."
+  copy_from_local, map_reduce, copy_to_local = create_job(config.to_hash())
   execute(copy_from_local, map_reduce, copy_to_local)
 
 
