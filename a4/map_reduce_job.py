6140bbab1f0a15987b06146021b10743458da35d
sacry- <nitsche25@gmail.com>
2014-12-04 12:47:08 +0100

    generalized map reduce paths

--- a4/map_reduce_job.py
+++ a4/map_reduce_job.py
@@ -1,14 +1,14 @@
+import os, sys
 
 def create_job(config):
   hadoop_dir = config["hadoop_dir"]
   path_to_streaming_jar = config["path_to_streaming_jar"]
-  nosql_dir = config["nosql_dir"]
   input_path = config["input_path"]
   output_path = config["output_path"]
   mapper = config["mapper"]
   reducer = config["reducer"]
 
-  copy_from_local = [hadoop_dir, "dfs", "-copyFromLocal", input_path, nosql_dir]
+  copy_from_local = [hadoop_dir, "dfs", "-copyFromLocal", input_path]
   map_reduce = [
       hadoop_dir, 
       "jar", path_to_streaming_jar, 
@@ -17,7 +17,7 @@ def create_job(config):
       "-input", input_path,
       "-output", output_path
   ]
-  copy_to_local = [haddop_dir, "dfs", "-copyToLocal", output_path, "./"]
+  copy_to_local = [hadoop_dir, "dfs", "-copyToLocal", output_path]
 
   return (copy_from_local, map_reduce, copy_to_local)
 
@@ -29,17 +29,17 @@ def execute(copy_from_local, map_reduce, copy_to_local):
   call(copy_to_local)
 
 
-nosql_dir = "user/nosql/"
+nosql_dir = os.path.dirname(os.path.realpath(__file__))
 config = {
-  "nosql_dir" : nosql_dir,
   "input_path" : "%sinput.txt" % nosql_dir,
   "output_path" : "%soutput.txt" % nosql_dir,
+  "mapper" : "%s/mapper.py" % nosql_dir,
+  "reducer" : "%s/reducer.py" % nosql_dir,
   "hadoop_dir" : "/usr/local/hadoop/bin/hadoop",
-  "path_to_streaming_jar" : "/usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.4.0.jar",
-  "mapper" : "./mapper.py",
-  "reducer" : "./reducer.py"
+  "path_to_streaming_jar" : "/usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.4.0.jar"
 }
 
+print "nosql dir: %s" % nosql_dir
 copy_from_local, map_reduce, copy_to_local = create_job(config)
 execute(copy_from_local, map_reduce, copy_to_local)
 
