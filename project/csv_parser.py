17edfd6c85af47a92e72a2556f256e9c5046ae7a
sacry- <nitsche25@gmail.com>
2014-12-12 22:07:48 +0100

    added csv parser for worldbank indicator for roughly 200mb data, added rediss adapter and bridge to worldbank

--- project/csv_parser.py
+++ project/csv_parser.py
@@ -1,5 +1,6 @@
 import csv
 import os, sys
+import re
 
 
 def parse_csv(csv_path, column_separator, quote_char):
@@ -8,10 +9,24 @@ def parse_csv(csv_path, column_separator, quote_char):
     for row in csv_reader:
       yield row
 
-def yield_files(csv_dir):
+def yield_files(csv_dir, match_pattern=".*"):
   from os import listdir
   from os.path import isfile, join
-  return [f for f in listdir(csv_dir) if isfile(join(csv_dir, f))]
+  pattern = re.compile(match_pattern)
+  result = []
+  for f in listdir(csv_dir):
+    if f.endswith(".csv") and isfile(join(csv_dir, f)):
+      m = pattern.search(f)
+      if m:
+        result.append(m.group())
+  return result
+
+def fetch_years(header):
+  years = []
+  for column in header:
+    if column.isdigit():
+      years.append(int(column))
+  return (years[0], years[-1])
 
 
 class WorldBankParser():
@@ -27,22 +42,15 @@ class WorldBankParser():
     return list(parse_csv(self.path_to_csv, self.sep, self.quote_char))
 
 
-class WorldBankData():
+class WorldIndicators():
 
   def __init__(self, csv_name):
     self.rows = WorldBankParser(csv_name).parse()
     self.title = self.rows[4][2]
     self.header_index = 2
     self.header = self.rows[self.header_index]
-    self.start_year, self.latest_year = self.__fetch_years__(self.header)
+    self.start_year, self.latest_year = fetch_years(self.header)
     self.data = self.__data__(self.rows)
-
-  def __fetch_years__(self, header):
-    years = []
-    for column in header:
-      if column.isdigit():
-        years.append(int(column))
-    return (years[0], years[-1])
 
   def __data__(self, rows):
     result = {}
@@ -55,9 +63,60 @@ class WorldBankData():
     return "WorldBankData<%s, %s-%s, size: %s>" % (self.title, self.start_year, self.latest_year, len(self.data))
 
 
-def test():
-  for f in yield_files("%s/world_bank" % os.path.dirname(os.path.realpath(__file__))):
-    print WorldBankData(f)
+class WorldBankWDI():
+
+  def __init__(self, csv_name):
+    self.rows = WorldBankParser(csv_name).parse()
+    self.header_index = 0
+    self.header = self.rows[self.header_index]
+    self.start_year, self.latest_year = fetch_years(self.header)
+    self.data = self.__data__(self.rows)
+
+  def __data__(self, rows):
+    result = {}
+    for row in rows[(self.header_index + 1):]:
+      country = row[0]
+      indicator = row[2]
+      sub_hash = dict(zip(self.header, row))
+      if result.has_key(indicator):
+        result[indicator][country] = sub_hash
+      else:
+        result[indicator] = { country : sub_hash }
+    return result
+
+  def __stats__(self):
+    sub_sizes = [reduce(lambda a,y: a+1, x, 0) for (i, x) in self.data.iteritems()]
+    sum_sizes = sum(sub_sizes) 
+    return (
+      len(self.data),
+      sum_sizes / len(sub_sizes),
+      sum_sizes
+    )
+
+  def __repr__(self):
+    stats = self.__stats__()
+    return "WorldBankWDI<%s-%s, indicators: %s, avg per indicator: %s, total: %s>" % (self.start_year, self.latest_year, stats[0], stats[1], stats[2])
 
 
-test()
+
+
+def indicators():
+  # http://data.worldbank.org/indicator/all
+  csv_dir = "%s/world_bank" % os.path.dirname(os.path.realpath(__file__))
+  for f in yield_files(csv_dir, ".*_[iI]ndicator_.*"):
+    yield WorldIndicators(f)
+
+def wdi_data():
+  # download first 140mb! 
+  # wget http://databank.worldbank.org/data/download/WDI_csv.zip
+  # unzip WDI_Data.csv into project/world_bank folder 
+  wdi_file = "WDI_Data.csv"
+  return WorldBankWDI(wdi_file)
+
+
+for indicator in indicators():
+  print indicator
+print wdi_data()
+
+
+
